190c98e9515d86193db06083c6c36d1256a6a63c
Iris Lopez Trujillo <iamiristhirteen13@gmail.com>
2024-10-20 18:31:37 +0200

    Arxius s'han afegit al projecte

---
+++ exercici01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace exercici01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracio varaible
+            int nota;
+
+            //valors inicials
+            Console.Write("Escriu una nota: ");
+            nota = Convert.ToInt32(Console.ReadLine());
+
+
+            //calculs
+            //Resultats
+            if (nota < 0 || nota > 10)
+                Console.WriteLine("Nota erronea");
+            else if (nota < 5)
+                Console.WriteLine("Suspès");
+            else if (nota < 7)
+                Console.WriteLine("Aprovat");
+            //si nota fos double seria else if (nota<9)
+            else if (nota == 7 || nota == 8)
+                Console.WriteLine("Notable");
+            else
+                Console.WriteLine("Excel·lent");
+        }
+    }
+}
